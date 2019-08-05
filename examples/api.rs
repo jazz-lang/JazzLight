7de2892fd390412fdc309148c4c57e44ea0b9788
playXE <adel.prokurov@protonmail.com>
2019-08-05 14:21:38 +0300

    Udpate examples

---
+++ examples/api.rs
@@ -0,0 +1,13 @@
+extern crate jazzc;
+
+use jazzc::State;
+
+fn main() {
+    let mut state = State::new();
+
+    state.set_var("x",vec![0,1,2,3]);
+
+    state.eval("x[2] = 4* 2").unwrap(); 
+
+    println!("{}",state.get_var("x").borrow());
+}
